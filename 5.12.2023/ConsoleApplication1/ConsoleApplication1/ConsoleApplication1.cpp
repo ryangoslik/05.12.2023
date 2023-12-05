c0d63941be330b68e84e5f36d6641976a01e4e5c
pawelce <szkolacecot@gmail.com>
2023-12-05 10:35:19 +0100

    123 sigma

---
+++ 5.12.2023/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int rozmiar;
+	cout << "podaj rozmiar tablicy imion = "; cin >> rozmiar;
+	string* imiona = new string[3];
+	for (size_t i = 0; i < rozmiar; i++)
+	{
+		cout << "podaj imie " << i << " = "; cin >> imiona[i];
+	}
+	for (size_t i = 0; i < 3; i++) {
+		if (i == 2) cout << imiona[i];
+		else cout << imiona[i] << ", ";
+	}
+	delete[] imiona;
+	return EXIT_SUCCESS;
+}
---
+++ 5.12.2023/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+int main()
+
+{
+    srand(time(NULL));
+    vector <int> tablica;
+    for (int i = 0; i < 5; i++) {
+        int los = rand() % 100 + 1;
+        tablica.push_back(los);
+    }
+    sort(tablica.begin(), tablica.end());
+    vector <int>::iterator nazwaIteratora = tablica.begin();
+    for (; nazwaIteratora != tablica.end(); nazwaIteratora++)
+    {
+        cout << *nazwaIteratora << endl;
+    }
+    return EXIT_SUCCESS;
+}
+
